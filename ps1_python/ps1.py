ff3fd19246e8a9a31c466cbb4671a3c5b4e0a400
Unknown <mjhaveri@bu.edu>
2017-12-25 23:15:51 -0500

    finished part a of problem 2

--- ps1_python/ps1.py
+++ ps1_python/ps1.py
@@ -1,22 +1,40 @@
 import cv2
-import numpy as np 
-import os 
+import numpy as np
+import os
+import math
+from PIL import Image
 
 
-def problem1(dir_path): 
+def problem1(dir_path):
     dir_path_prob_1 = dir_path + '/input/' + 'ps1-input0.png'
     img = cv2.imread(dir_path_prob_1, 0)
     edges = cv2.Canny(img, 0, 250)
     dir_path_prob_1_save_path = dir_path + '/output/' + 'ps1-1-a-1.png'
-    print(dir_path_prob_1_save_path)
     cv2.imwrite(dir_path_prob_1_save_path, edges)
-    #cv2.imshow('image', edges)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows() 
     return edges
 
-def problem2(edges): 
-    pass 
 
-dir_path = os.path.dirname(os.path.realpath(__file__)) + '/' + 'ps1_octave_template'
+def problem2(edges, theta_bin, d_bin):
+    height, width = edges.shape
+    numTheta = math.ceil(181 / theta_bin)
+    d_len = math.ceil(((height**2) + (width**2))**(1 / 2))
+    numD = math.ceil(d_len)
+    hought_arr = np.zeros((numD, numTheta))
+    theta_arr = []
+    edge_arr = [(i, j) for i in range(height)
+                for j in range(width) if edges[i, j]]
+
+    for i, j in edge_arr:
+        theta = np.arctan2(i, j)
+        theta_ang = math.floor(theta * (180 / np.pi))
+        p = math.floor(i * np.sin(theta) + j * np.cos(theta))
+        hought_arr[p, theta_ang] += 1
+
+    print(np.amax(hought_arr))
+
+
+dir_path = os.path.dirname(os.path.abspath(
+    __file__)) + '/' + 'ps1_octave_template'
+
 edges = problem1(dir_path)
+problem2(edges, 1, 1)
